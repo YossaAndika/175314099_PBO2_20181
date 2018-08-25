6c528408aeb699240e281936dafc065b2e9db71e
windows10 <windows10@Yoandika>
2018-08-25 19:51:24 +0700

    testPasien dengan rekam medis 2 099

--- src/test/TestPasien1.java
+++ src/test/TestPasien1.java
@@ -17,8 +17,23 @@ public class TestPasien1 {
     public static void main(String[] args) {
         Pasien puspa = new Pasien("Puspa");
         puspa.setTanggalLahir(1974, 1, 12);
-        System.out.println("Umur = "+puspa.getUsia());
-        System.out.println("Rekam Medis = "+puspa.setTanggalDaftar()+""+puspa.setKodeNama());
+        puspa.setAlamat("Klaten");
+        puspa.setTempatLahir("Klaten");
+        
+//        System.out.println("Rekam Medis = "+puspa.setTanggalDaftar()+""+puspa.setKodeNama());
+//        puspa.Mendaftar();
+        
+        System.out.println("-------------------------------------");
+        System.out.println("\tNomor Antrian Pasien");
+        System.out.print("\t    "); puspa.Mendaftar();
+        System.out.println("-------------------------------------");
+        
+        System.out.println("Nama          : "+puspa.getNama());
+        System.out.println("Umur          : "+puspa.getUsia());
+        System.out.println("Tanggal Lahir : "+puspa.getTanggalLahir());
+        System.out.println("Teampat Lahir : "+puspa.getTempatLahir());
+        System.out.println("Alamat        : "+puspa.getAlamat());
+        System.out.println("");
     }
     
 }
