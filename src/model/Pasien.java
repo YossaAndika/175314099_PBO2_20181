09925d2106ec449f19097a58b7da111c8ed597de
windows10 <windows10@Yoandika>
2018-09-01 14:12:17 +0700

    Pasien v.2

--- src/model/Pasien.java
+++ src/model/Pasien.java
@@ -15,10 +15,24 @@ import java.util.Date;
  */
 public class Pasien {
     private String nama;
-    private Date tanggalLahir;
+    private int tanggalLahir;
     private String alamat;
     private String tempatLahir;
+    private int bulanLahir;
+    private int tahunLahir;
     private String RekamMedis;
+    
+    public Pasien(){
+        
+    }
+    
+    public Pasien(String nama){
+        this.nama=nama;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
 
     public String getAlamat() {
         return alamat;
@@ -40,61 +54,57 @@ public class Pasien {
         return RekamMedis;
     }
 
-    public void setRekamMedis(String RekamMedis) {
-        this.RekamMedis = RekamMedis;
+    public void setRekamMedis(String RekamMedis) throws Exception {
+        if(this.RekamMedis.length() > 6){
+            this.RekamMedis = RekamMedis;
+        }else{
+            throw new Exception("Nomor Yang Anda Masukan Salah");
+        }
     }
     
-    
-
-    public Pasien(String nama) {
-        this.nama = nama;
-    }
-
-    /**
-     * @return the nama
-     */
-    public String getNama() {
-        return nama;
-    }
-
-    /**
-     * @param nama the nama to set
-     */
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    /**
-     * @return the tanggalLahir
-     */
-    public Date getTanggalLahir() {
+    public int getTanggalLahir() {
         return tanggalLahir;
     }
 
-    /**
-     * @param tanggalLahir the tanggalLahir to set
-     */
-    public void setTanggalLahir(Date tanggalLahir) {
-        this.tanggalLahir = tanggalLahir;
+    public void setTanggalLahir(int tanggalLahir)throws Exception {
+        if(tanggalLahir > 0){
+        if(tanggalLahir <= 31){
+           this.tanggalLahir = tanggalLahir;
+        }else{ throw new Exception ("Tanggal yang Anda masukan melebihi batas yang ditentukan");}
+    }else{ throw new Exception ("Tanggal yang Anda masukan kurang dari batas yang ditentukan ");
+    
+        }
     }
     
-    /**
-     * 
-     * @param tahun
-     * @param bulan
-     * @param tanggal 
-     */
-    public void setTanggalLahir(int tahun, int bulan, int tanggal) {
-        tanggalLahir = new Date(tahun-1900,bulan-1,tanggal);
+    public int getbulanLahir(){
+        return bulanLahir;
     }
     
-    public int getUsia(){
-        Date today = new Date();
-        return today.getYear() - tanggalLahir.getYear();
+    public void setbulanLahir(int bulanLahir)throws Exception{
+        if(bulanLahir > 0){
+        if(bulanLahir <= 12){
+           this.bulanLahir = bulanLahir;
+        }else{ throw new Exception ("Bulan yang Anda masukan melebihi batas yang ditentukan");
+        
+        }
+    }else{ throw new Exception ("Bulan yang Anda masukan kurang dari batas yang ditentukan ");
+        }   
+    }
+
+    
+    
+    public int gettahunLahir(){
+        return tahunLahir;
     }
     
+    public void settahunLahir(int tahunLahir)throws Exception{
+        if(tahunLahir > 0){
+           this.tahunLahir = tahunLahir;
+        }else{ throw new Exception ("Tahun yang diinputkan salah");
+        }
+    }
     
-    public void Mendaftar(){
+    public String Mendaftar(){
       Date date = new Date();
       RekamMedis= "yyyyMMdd";
       SimpleDateFormat ft = 
@@ -102,7 +112,8 @@ public class Pasien {
       
       String sub_nama = nama.substring(0, 3);
       
-        String RekamMedis = ft.format(date).concat(sub_nama);
-        System.out.println(RekamMedis);
+        return RekamMedis = ft.format(date).concat(sub_nama);
     }
+
+
 }
