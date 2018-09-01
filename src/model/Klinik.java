6eedb051bd0a49053ca1e1f5ae72515de3ae60a0
windows10 <windows10@Yoandika>
2018-09-01 14:11:29 +0700

    Klinik

---
+++ src/model/Klinik.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author windows10
+ */
+public class Klinik {
+    private String namaKlinik;
+    private String idKlinik;
+    
+    public Klinik(){
+        
+    }
+
+    public String getNamaKlinik() {
+        return namaKlinik;
+    }
+
+    public void setNamaKlinik(String namaKlinik) {
+        this.namaKlinik = namaKlinik;
+    }
+
+    public String getIdKlinik() {
+        return idKlinik;
+    }
+
+    public void setIdKlinik(String idKlinik) {
+        this.idKlinik = idKlinik;
+    }
+    
+    
+}
