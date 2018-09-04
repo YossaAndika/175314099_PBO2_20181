c746cfa89cddc238962453d6a6eec38d09bb12dd
windows10 <windows10@Yoandika>
2018-09-04 17:20:53 +0700

    dokter v.2

--- src/model/Dokter.java
+++ src/model/Dokter.java
@@ -10,13 +10,19 @@ package model;
  * @author windows10
  */
 public class Dokter {
-
+    //deklarsi variabel nama bertipe String dengan sifat private
     private String nama;
+    //deklarsi variabel alamt bertipe String dengan sifat private
     private String alamat;
+    //deklarsi variabel noPegawai bertipe String dengan sifat private
     private String noPegawai;
+    //deklarsi variabel tenpatLahir bertipe String dengan sifat private
     private String tempatLahir;
+    //deklarsi variabel tanggalLahir bertipe integer dengan sifat private
     private int tanggalLahir;
+    //deklarsi variabel bulanLahir bertipe integer dengan sifat private
     private int bulanLahir;
+    //deklarsi variabel tahunLahir bertipe integer dengan sifat private
     private int tahunLahir;
 
     public Dokter() {
@@ -43,10 +49,18 @@ public class Dokter {
         return noPegawai;
     }
 
+    /***
+     * method ini digunakan untuk memasukan nomor pegawai
+     * dokter
+     * @param noPegawai
+     * @throws Exception 
+     */
     public void setNoPegawai(String noPegawai) throws Exception {
+        //panjang digit nomor pegawai harus sama dengan 5
         if (noPegawai.length() == 5) {
             this.noPegawai = noPegawai;
         } else {
+            //nomor pegawai yang dimasukan kurang atau lebigh dari 5 digit
             throw new Exception("Nomor yang Anda masukan salah");
         }
     }
@@ -63,13 +77,22 @@ public class Dokter {
         return tanggalLahir;
     }
 
+    /***
+     * method ini digunakan untuk mengatur tanggal lahir dokter
+     * @param tanggalLahir
+     * @throws Exception 
+     */
     public void setTanggalLahir(int tanggalLahir) throws Exception {
+        //tanggal yang dimasukan tidak boleh kurang dari 1 atau harus lebih dari 0
         if (tanggalLahir > 0) {
+            //tanggal yang dimasukan tidak boleh lebih dari 31
             if (tanggalLahir <= 31) {
                 this.tanggalLahir = tanggalLahir;
+                //tanggal yang dimasukan lebih dari 31
             } else {
                 throw new Exception("Tanggal yang Anda masukan melebihi batas yang ditentukan");
             }
+            //tanggal yang dimasukan kurang dari 1
         } else {
             throw new Exception("Tanggal yang Anda masukan kurang dari batas yang ditentukan ");
 
@@ -80,14 +103,23 @@ public class Dokter {
         return bulanLahir;
     }
 
+    /***
+     * method ini digunakan untuk mengatur bulan lahir dari dokter
+     * @param bulanLahir
+     * @throws Exception 
+     */
     public void setbulanLahir(int bulanLahir) throws Exception {
+        //bulan lahir yang dimasukan tidak boleh kurang dari 1 atau harus lebih dari 0
         if (bulanLahir > 0) {
+            //bulan lahir yang dimasukan tidak boleh lebih dari 12
             if (bulanLahir <= 12) {
                 this.bulanLahir = bulanLahir;
+                //bulan lahir yang dimasukan lebih dari 12
             } else {
                 throw new Exception("Bulan yang Anda masukan melebihi batas yang ditentukan");
 
             }
+            //bulan lahir yang dimasukan kurang dari 1
         } else {
             throw new Exception("Bulan yang Anda masukan kurang dari batas yang ditentukan ");
         }
@@ -97,9 +129,16 @@ public class Dokter {
         return tahunLahir;
     }
 
+    /***
+     * method ini digunakan untuk mengatur tanggal lahir dari dokter
+     * @param tahunLahir
+     * @throws Exception 
+     */
     public void settahunLahir(int tahunLahir) throws Exception {
+        //tahun lahir yang dimasukan tidak boleh kurang dari 1 atau harus lebih dari 0
         if (tahunLahir > 0) {
             this.tahunLahir = tahunLahir;
+            //tahun lahir yang dimasukan kurang dari 1
         } else {
             throw new Exception("Tahun yang diinputkan salah");
         }
