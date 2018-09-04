2dfa5ac9d51d637db108d27335ce0d34de7874e3
windows10 <windows10@Yoandika>
2018-09-04 17:18:19 +0700

    Pasien v.2.1

--- src/model/Pasien.java
+++ src/model/Pasien.java
@@ -53,10 +53,19 @@ public class Pasien {
     public String getRekamMedis() {
         return RekamMedis;
     }
+    
+    /***
+     * method ini digunakan untuk mengatur panjang nomor rekam medis dengan
+     * menggunakan fungsi exception
+     * @param RekamMedis
+     * @throws Exception 
+     */
 
     public void setRekamMedis(String RekamMedis) throws Exception {
+        //panjang rekam medis yang harus dimasukan lebih dari 6
         if(this.RekamMedis.length() > 6){
             this.RekamMedis = RekamMedis;
+            //jika panjang rekam medis yang dimasukan kurang dari dari 6
         }else{
             throw new Exception("Nomor Yang Anda Masukan Salah");
         }
@@ -65,12 +74,22 @@ public class Pasien {
     public int getTanggalLahir() {
         return tanggalLahir;
     }
+    
+    /***
+     * method ini digunkan untuk mengatur tanggal lahir dari pasien
+     * @param tanggalLahir
+     * @throws Exception 
+     */
 
     public void setTanggalLahir(int tanggalLahir)throws Exception {
+        //tanggal lahir yang dimasukan tidak boleh kurang dari 1 atau harus lebih dari 0
         if(tanggalLahir > 0){
+            //tanggal lahir yang dimasukan tidak boleh lebih dari 31
         if(tanggalLahir <= 31){
            this.tanggalLahir = tanggalLahir;
+           //jika tanggal lahir yang dimasukan lebih dari 31
         }else{ throw new Exception ("Tanggal yang Anda masukan melebihi batas yang ditentukan");}
+        //jika tanggal lahir yang dimasukan kurang dari 1
     }else{ throw new Exception ("Tanggal yang Anda masukan kurang dari batas yang ditentukan ");
     
         }
@@ -80,13 +99,22 @@ public class Pasien {
         return bulanLahir;
     }
     
+    /***
+     * method ini digunakan untuk mengatur bulan lahir dari pasien
+     * @param bulanLahir
+     * @throws Exception 
+     */
+    
     public void setbulanLahir(int bulanLahir)throws Exception{
+        //bulan lahir yang dimasukan tidak boleh kurang dari 1 atau harus lebih dari 0
         if(bulanLahir > 0){
+            //bulan lahir yang dimasukan tidak boleh lebih dari 12
         if(bulanLahir <= 12){
            this.bulanLahir = bulanLahir;
+           //jika bulan lahir yang dimasukan lebih dari 12
         }else{ throw new Exception ("Bulan yang Anda masukan melebihi batas yang ditentukan");
         
-        }
+        }//jika bulan lahir yang dimasukan kurang dari 1
     }else{ throw new Exception ("Bulan yang Anda masukan kurang dari batas yang ditentukan ");
         }   
     }
@@ -97,12 +125,26 @@ public class Pasien {
         return tahunLahir;
     }
     
+    /***
+     * method ini digunakan untuk mengatur tahun lahir pasien
+     * @param tahunLahir
+     * @throws Exception 
+     */
+    
     public void settahunLahir(int tahunLahir)throws Exception{
+        //tahun lahir yang dimasukan tidak boleh kurang dari 1 atau harus lebih dari 0
         if(tahunLahir > 0){
            this.tahunLahir = tahunLahir;
+           //jika tahun lahir yang dimasukan kurang dari 1
         }else{ throw new Exception ("Tahun yang diinputkan salah");
         }
     }
+    
+    /***
+     * method ini digunakan untuk mengatur foramat rekam medis pasien dengan menggunakan
+     * tanggal mendaftar dan 3 huruf pertama dari nama pasien
+     * @return 
+     */
     
     public String Mendaftar(){
       Date date = new Date();
@@ -117,3 +159,4 @@ public class Pasien {
 
 
 }
+
