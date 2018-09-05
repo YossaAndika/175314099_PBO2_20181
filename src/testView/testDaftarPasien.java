b9cb943a3a6c46cf924e7e6a8368151777b90b65
windows10 <windows10@Yoandika>
2018-09-05 11:55:44 +0700

    TestDaftarPasien

---
+++ src/testView/testDaftarPasien.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testView;
+
+import view.DaftarPasien;
+
+/**
+ *
+ * @author windows10
+ */
+public class testDaftarPasien {
+    public static void main(String[] args) {
+        DaftarPasien test = new DaftarPasien();
+        test.setSize(300, 400);
+        test.setVisible(true);
+    }
+}
