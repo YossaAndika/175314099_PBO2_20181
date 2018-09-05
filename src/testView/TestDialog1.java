3aa0065dfd18fcfb83ae946dae3610256417b271
windows10 <windows10@Yoandika>
2018-09-05 11:55:25 +0700

    testDialog

---
+++ src/testView/TestDialog1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testView;
+
+import view.LatihanDialog;
+
+/**
+ *
+ * @author user only
+ */
+public class TestDialog1 {
+    public static void main(String[] args) {
+        LatihanDialog test = new LatihanDialog("ini latihan");
+        test.setSize(300, 400);
+        test.setVisible(true);
+    }
+}
